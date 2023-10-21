e01feb8ff41337b642f0ec12eabbb679f18322cd
NeverMore <uragan.uragan.59@mail.ru>
2023-10-21 15:35:14 +0300

    add get functions

--- connections/tables/client_agreement.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS connections.client_agreement
-(
-    id_client    integer NOT NULL,
-    id_agreement integer NOT NULL
-);
---
+++ humanresource/functions/get_employee_project.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION humanresource.get_employee_project(_log_id BIGINT) RETURNS JSON
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+AS
+$$
+DECLARE
+    _res JSONB;
+BEGIN
+    RETURN JSONB_BUILD_OBJECT('data', json_agg(row_to_json(res)))
+        FROM (select a.id, a.text, a.date_start, a.date_end, a.id_client
+              from humanresource.employee e
+                       join connections.employee_project ep on e.employee_id = ep.id_employee
+                       join projects.agreement a on a.id = ep.id_agreement
+              where e.employee_id = _log_id) res;
+
+END
+$$;
